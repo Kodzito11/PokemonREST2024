0c5f83b256e7562bca8aae618197f988d0f4044f
Kodzito11 <Kos001@edu.zealand.dk>
2024-11-13 10:59:26 +0100

    New update, because projects cannot be seperate when delivering to azure

---
+++ PokemonREST2024/PokemonLIb/Pokemon.cs
@@ -0,0 +1,23 @@
+﻿namespace PokemonREST2024.PokemonLIb
+{
+
+		public class Pokemon
+		{
+			public int Id { get; set; }
+			public string? Name { get; set; }
+			public int Level { get; set; }
+			public int PokeDex { get; set; }
+
+			public void ValidateName()
+			{
+				if (Name == null) throw new ArgumentNullException();
+				if (Name.Length <= 2) throw new ArgumentOutOfRangeException("specifik besked");
+			}
+
+			public void Validate()
+			{
+				ValidateName();
+			}
+		}
+
+}
